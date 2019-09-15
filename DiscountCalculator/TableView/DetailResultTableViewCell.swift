ccf42c8ed77ad2236e795844cfe94b854d80c8a2
William Santoso <williamsantoso93@yahoo.com>
2019-09-15 12:07:15 +0700

    detail result

---
+++ DiscountCalculator/TableView/DetailResultTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  DetailResultTableViewCell.swift
+//  DiscountCalculator
+//
+//  Created by William Santoso on 15/09/19.
+//  Copyright © 2019 William Santoso. All rights reserved.
+//
+
+import UIKit
+
+class DetailResultTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
